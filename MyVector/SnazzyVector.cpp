6c527b82c0a240efda989f8cfa823ebf4262ee7b
Nikhil Razdan <39114568+nsrazdan@users.noreply.github.com>
2019-04-05 22:50:59 -0700

    organizing

---
+++ MyVector/SnazzyVector.cpp
@@ -0,0 +1,5 @@
+#include "SnazzyVector.h"
+
+namespace Snazzy {
+    
+}
---
+++ MyVector/SnazzyVector.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+namespace Snazzy {
+    class vector {
+        public:
+            vector() : size(0)
+        private:
+            unsigned int size;
+    };
+}
